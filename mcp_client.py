395c228fc63cbc514c7b54232f473507618cf2ae
Nathan Anderson <nate@nathanlukeanderson.com>
2026-05-18 17:27:40 -0600

    Updated the client skills

--- mcp_client.py
+++ mcp_client.py
@@ -48,8 +48,7 @@ class MCPClient:
     async def call_tool(
         self, tool_name: str, tool_input: dict
     ) -> types.CallToolResult | None:
-        # TODO: Call a particular tool and return the result
-        return None
+        return await self.session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
         # TODO: Return a list of prompts defined by the MCP server
